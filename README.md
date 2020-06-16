b05518cc84ef5693017170abe5274415da000c5e
Leonardo Castorina <leonardo.castorina98@gmail.com>
2020-06-16 22:01:19 +0200

    #27 Add bash script to build dataset from videos

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Tiredness Analysis
-Script allowing to make a detailed analysis of changes within the eyes during the work time.
+
+**Goal**: identify and analyze different tiredness
+ patterns that can be spotted from face-centered videos.
+
+
+- [WP01: Processing videos from the UTA-RLDD dataset](work-products/WP01/)
+
+
---
+++ work_products/WP01/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+for person_id_folder in video/*; do
+  for video_class in $person_id_folder/*; do
+    person_id="$(cut -d'/' -f2 <<<"$video_class")"
+    class_id="$(cut -d'/' -f3 <<<"$video_class")"
+    class_id="${class_id%.*}"
+    output_filename=output/$person_id'_'$class_id'.csv'
+    python main.py process_videos $video_class $output_filename &
+    done
+done
+wait
